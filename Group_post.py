9f0c3e1a23dfc92b3bc381ba61c961faa6c24236
hariharanrangasamy <hariharanrangasamy@gmail.com>
2013-10-04 23:17:25 +0530

    formatted to pep8

--- Group_post.py
+++ Group_post.py
@@ -6,34 +6,38 @@ import sys
     To get the access token goto http://graph.facebook.com/tools/explorer
     With "Get Access Token" option select "user_group" and "Friend_group" in "User Data Permissions" and "Friends Data Permissions"
 """
-TOKEN = '<access token>' 
+TOKEN = '<access token>'
 
-#Collects all group names and group id from the user which he belongs to
+# Collects all group names and group id from the user which he belongs to
+
+
 def get_groups():
-    
-    query = ("SELECT gid,name FROM group where gid in ( SELECT gid FROM group_member WHERE uid=me())")
-    payload = {'q':query,'access_token': TOKEN}
+
+    query = (
+        "SELECT gid,name FROM group where gid in ( SELECT gid FROM group_member WHERE uid=me())")
+    payload = {'q': query, 'access_token': TOKEN}
     response = requests.get('https://graph.facebook.com/fql', params=payload)
     result = json.loads(response.text)
     return result['data']
 
-#Post feeds to all the groups
+# Post feeds to all the groups
+
+
 def Post_status(name):
-    message=raw_input("Enter your message to be posted:")
+    message = raw_input("Enter your message to be posted:")
     for i in name:
         try:
             prompt = u"Want to post in %s?(Y/N) " % i['name']
             check = raw_input(prompt.encode(sys.stdout.encoding))
         except UnicodeError:
             continue
-        if ((check=='N')|(check=='n')):
+        if ((check == 'N') | (check == 'n')):
             continue
-        post_data = {'access_token':TOKEN, 'message':message}
-        url='https://graph.facebook.com/%s/feed' % str(i['gid'])
-        response = requests.post(url,data=post_data)
-        
+        post_data = {'access_token': TOKEN, 'message': message}
+        url = 'https://graph.facebook.com/%s/feed' % str(i['gid'])
+        response = requests.post(url, data=post_data)
+
 if __name__ == '__main__':
     Post_status(get_groups())
-    
-#To run the script "python group_post.py"
-    
+
+# To run the script "python group_post.py"
